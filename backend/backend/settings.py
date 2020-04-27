21559a9da816e1d43c519687ff47e52e39cc0794
Bruno <oliveiradantas96@gmail.com>
2020-04-26 22:56:32 -0300

    Adicionando migrations

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'sevenapp',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,7 +51,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:4200',
+)
 
 ROOT_URLCONF = 'backend.urls'
 
--- backend/sevenapp/models.py
+++ backend/sevenapp/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class User(models.Model):
     
-    user_id = models.CharField(max_length=150, blank=False, unique=True)
+    user_id = models.AutoField(primary_key=True, unique =True)
     name = models.CharField(max_length=60)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique = True)
-    _password = models.CharField(max_length = 254)
+    password = models.CharField(max_length = 254)
